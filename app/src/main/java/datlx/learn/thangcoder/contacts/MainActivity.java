de03645378a375066c16a0d429f7499dce77a5e0
Luu Xuan Dat <datlx@deha-soft.com>
2022-03-12 16:50:54 +0700

    Step 3: Call custom adapter

--- app/src/main/java/datlx/learn/thangcoder/contacts/MainActivity.java
+++ app/src/main/java/datlx/learn/thangcoder/contacts/MainActivity.java
@@ -1,14 +1,70 @@
 package datlx.learn.thangcoder.contacts;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+import datlx.learn.thangcoder.contacts.adapters.ContactAdapter;
+import datlx.learn.thangcoder.contacts.models.Contact;
 
 public class MainActivity extends AppCompatActivity {
+    private List<Contact> arrayContact;
+    private ContactAdapter adapter;
+    private EditText edtName;
+    private EditText edtNumber;
+    private RadioButton rbtnMale;
+    private RadioButton rbtnFemale;
+    private Button btnAddContact;
+    private ListView lvContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setWidget();
+
+        arrayContact = new ArrayList<>();
+        adapter = new ContactAdapter(this, R.layout.item_contact_listview, arrayContact);
+        lvContact.setAdapter(adapter);
+    }
+
+    private void setWidget() {
+        edtName = (EditText) findViewById(R.id.edt_name);
+        edtNumber = (EditText) findViewById(R.id.edt_phone);
+        rbtnMale = (RadioButton) findViewById(R.id.rbtn_female);
+        rbtnFemale = (RadioButton) findViewById(R.id.rbtn_female);
+        btnAddContact = (Button) findViewById(R.id.btn_add_contact);
+        lvContact = (ListView) findViewById(R.id.lv_contacts);
+    }
+
+    public void btnAddContactClick(View view) {
+        if (view.getId() == R.id.btn_add_contact) {
+            String name = edtName.getText().toString().trim();
+            String number = edtNumber.getText().toString().trim();
+            boolean isMale = true;
+            if (rbtnMale.isChecked()) {
+                isMale = true;
+            } else {
+                isMale = false;
+            }
+            if (TextUtils.isEmpty(name) || TextUtils.isEmpty(number)) {
+                Toast.makeText(this, "Please Input Number or Name", Toast.LENGTH_SHORT).show();
+            } else {
+                Contact contact = new Contact(name, number, isMale);
+                arrayContact.add(contact);
+            }
+            adapter.notifyDataSetChanged();
+        }
     }
 }
