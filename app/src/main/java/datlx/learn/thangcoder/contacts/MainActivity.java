a6570af9e03ff857242d54625704eb5d831d437a
Luu Xuan Dat <datlx@deha-soft.com>
2022-03-12 18:46:03 +0700

    01 - Call SMS Phone , Send SMS

--- app/src/main/java/datlx/learn/thangcoder/contacts/MainActivity.java
+++ app/src/main/java/datlx/learn/thangcoder/contacts/MainActivity.java
@@ -1,8 +1,14 @@
 package datlx.learn.thangcoder.contacts;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -10,6 +16,8 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +45,63 @@ public class MainActivity extends AppCompatActivity {
         arrayContact = new ArrayList<>();
         adapter = new ContactAdapter(this, R.layout.item_contact_listview, arrayContact);
         lvContact.setAdapter(adapter);
+
+        checkAndRequestPermissions();
+        lvContact.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showDialogConfirm(position);
+            }
+        });
+    }
+
+    private void showDialogConfirm(int position) {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_dialog_layout);
+        Button btnCall = (Button) dialog.findViewById(R.id.btn_call);
+        Button btnSendMessage = (Button) dialog.findViewById(R.id.btn_send_message);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentCall(position);
+            }
+        });
+        btnSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentSendMessage(position);
+            }
+        });
+        dialog.show();
+    }
+
+    private void intentCall(int position) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + arrayContact.get(position).getmPhone()));
+        startActivity(intent);
+    }
+
+    private void intentSendMessage(int position) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + arrayContact.get(position).getmPhone()));
+        startActivity(intent);
+    }
+
+    private void checkAndRequestPermissions() {
+        String[] permissions = new String[]{
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.SEND_SMS
+        };
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(permission);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 1);
+        }
     }
 
     private void setWidget() {
@@ -52,12 +117,7 @@ public class MainActivity extends AppCompatActivity {
         if (view.getId() == R.id.btn_add_contact) {
             String name = edtName.getText().toString().trim();
             String number = edtNumber.getText().toString().trim();
-            boolean isMale = true;
-            if (rbtnMale.isChecked()) {
-                isMale = true;
-            } else {
-                isMale = false;
-            }
+            boolean isMale = rbtnMale.isChecked();
             if (TextUtils.isEmpty(name) || TextUtils.isEmpty(number)) {
                 Toast.makeText(this, "Please Input Number or Name", Toast.LENGTH_SHORT).show();
             } else {
